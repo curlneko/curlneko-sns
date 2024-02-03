adb53b6bdc6aea20555589fdcf723e9fd43f9066
yufangling <fang.ling.yu@beex-inc.com>
2024-02-03 21:38:06 +0900

    prismaを導入、mysqlと疎通確認

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -1,16 +1,24 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
+const {PrismaClient} = require("@prisma/client");
+
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  // res.send('respond with a resource');
-  res.json([{
-    id: 1,
-    name: "yujiro1"
-  }, {
-    id: 2,
-    name: "yuziro2"
-  }]);
+// router.get('/', function(req, res, next) {
+//   // res.send('respond with a resource');
+//   res.json([{
+//     id: 1,
+//     name: "yujiro1"
+//   }, {
+//     id: 2,
+//     name: "yuziro2"
+//   }]);
+// });
+
+router.get("/", async (req, res, next) => {
+  const prisma = new PrismaClient();
+  const users = await prisma.user.findMany();
+  res.json(users);
 });
 
 module.exports = router;
